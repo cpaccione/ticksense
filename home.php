829024e6f602b063811aeabc7f393aa343d3f972
Christopher Paccione <frost1177@gmail.com>
2015-05-18 23:32:41 -0500

    added excerpt
    - home.php

--- home.php
+++ home.php
@@ -15,10 +15,12 @@
         <div class="col-md-8">
 
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
             <div class="title">
                 <img class="blog-image" src="<?php bloginfo('stylesheet_directory');?>/images/blog_post_logo.png">
                 <h1 class="blog-title"><?php the_title(); ?></h1>
             </div>
+            <p><?php the_excerpt(); ?><p>
             <p>
                 By <?php the_author(); ?>
                 on <?php echo the_time('l, F jS, Y');?>
@@ -26,11 +28,11 @@
                 <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a>
             </p>
 
-            <?php the_content(); ?>
+            <?php //the_content(); ?>
             <hr>
             <?php comments_template(); ?>
 
-            <?php endwhile; else: ?>
+        <?php endwhile; else: ?>
 
                 <div class="page-header">
                     <h1>Oh no!</h1>
@@ -38,13 +40,14 @@
 
                 <p>No content is appearing on this page!</p>
 
-            <?php endif; ?>
-        </div>
+        <?php endif; ?>
+
+        </div> <!-- col-md-8 end -->
 
         <div class="col-md-4">
             <?php get_sidebar( 'blog' ); ?>
-        </div>
+        </div> <!--col-md-4 end -->
 
-        </div>
+        </div> <!-- row end -->
 
 <?php get_footer(); ?>
