a1d15b8b7f8ed37b9b07cf18f621b6238a32afb8
mpaccione <mpaccione@comcast.net>
2015-05-23 11:16:46 -0700

    Added code from 2014Theme -> pulled.php
    Copy pasted relevant code

---
+++ pulled.php
@@ -0,0 +1,96 @@
+<!-- Pulled Code -->
+
+<!-- featured-content.php -->
+
+<div class="featured-content-inner">
+	<?php
+		/**
+		 * Fires before the Twenty Fourteen featured content.
+		 */
+		do_action( 'twentyfourteen_featured_posts_before' );
+
+		$featured_posts = twentyfourteen_get_featured_posts();
+		foreach ( (array) $featured_posts as $order => $post ) :
+			setup_postdata( $post );
+
+			 // Include the featured content template.
+			get_template_part( 'content', 'featured-post' );
+		endforeach;
+
+		/**
+		 * Fires after the Twenty Fourteen featured content.
+		 */
+		do_action( 'twentyfourteen_featured_posts_after' );
+
+		wp_reset_postdata();
+	?>
+</div>
+
+
+<!-- content-featured-post.php -->
+
+<?php
+/**
+ * The template for displaying featured posts on the front page
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
+	<?php
+		// Output the featured image.
+		if ( has_post_thumbnail() ) :
+			if ( 'grid' == get_theme_mod( 'featured_content_layout' ) ) {
+				the_post_thumbnail();
+			} else {
+				the_post_thumbnail( 'twentyfourteen-full-width' );
+			}
+		endif;
+	?>
+	</a>
+
+	<header class="entry-header">
+		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
+		<div class="entry-meta">
+			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+
+		<?php the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">','</a></h1>' ); ?>
+	</header>
+</article><!-- #post-## -->
+
+
+<!-- functions.php -->
+<?php
+
+// Add support for featured content.
+	add_theme_support( 'featured-content', array(
+		'featured_content_filter' => 'twentyfourteen_get_featured_posts',
+		'max_posts' => 6,
+	) );
+
+function twentyfourteen_get_featured_posts() {
+	/**
+	 * Filter the featured posts to return in Twenty Fourteen.
+	 *
+	 * @since Twenty Fourteen 1.0
+	 *
+	 * @param array|bool $posts Array of featured posts, otherwise false.
+	 */
+	return apply_filters( 'twentyfourteen_get_featured_posts', array() );
+}
+
+/**
+ * A helper conditional function that returns a boolean value.
+ *
+ * @since Twenty Fourteen 1.0
+ *
+ * @return bool Whether there are featured posts.
+ */
+function twentyfourteen_has_featured_posts() {
+	return ! is_paged() && (bool) twentyfourteen_get_featured_posts();
+}
+
+
+?>
