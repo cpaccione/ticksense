adb146438976b845cfb93f27971de768194fa8e0
mpaccione <mpaccione@comcast.net>
2015-05-24 15:55:21 -0700

    Fixed Banner and WIP Rollovers
    Added Banner sizing for the sixth post and added rollovers. Rollovers
    need to be positioned properly.

--- masonry.php
+++ masonry.php
@@ -1,21 +1,29 @@
+<style type="text/css">
+	.imagetitle {
+		opacity: 0;
+	}
+	.imagetitle:hover {
+		opacity: 1;
+	}
+</style>
 <?php
+
+
 $featured_posts = new WP_Query(array(
 	'cat'=>'featured',
 	'posts_per_page'=>6,
 ));
-$sizes = array('2x2', '1x1', '2x1', '2x1', '1x1',);
+$sizes = array('2x2', '1x1', '2x1', '2x1', '1x1', '2x1');
 ?>
 <div class="featured-masonry">
 	<ul>
 		<?php $i = 0; while ($featured_posts->have_posts()) : $featured_posts->the_post(); ?>
 
 			<li class="masonry-item">
-				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('masonry_' . $sizes[$i % count($sizes)]); ?></a>
+				<a href="<?php the_permalink(); ?>"><span class='imagetitle'><?php the_title(); ?></span><?php the_post_thumbnail('masonry_' . $sizes[$i % count($sizes)]); ?></a>
 			</li>
 
 		<?php $i ++; endwhile; ?>
 		<?php wp_reset_postdata(); ?>
 	</ul>
 </div>
-
-<!--<?php the_title(); ?>-->
